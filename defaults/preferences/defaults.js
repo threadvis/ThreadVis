7d1484d0a94f14b77ac122adf68c502cfc300cd9
Alexander C. Hubmann-Haidvogel <ahubmann@gmail.com>
2005-05-09 16:10:43 +0000

    Settings.xul: - added prefdefvalue
    defaults/preferences/defaults.js:
    - added default preferences
    
    make.bat:
    - added default preferences

---
+++ defaults/preferences/defaults.js
@@ -0,0 +1,14 @@
+/* *******************************************************
+ * defaults.js
+ *
+ * (c) 2005 Alexander C. Hubmann
+ *
+ * default preferences
+ *
+ * Version: $Id$
+ ********************************************************/
+
+pref("extensions.threadarcsjs.logging.debug", false);
+pref("extensions.threadarcsjs.logging.enabled", false);
+pref("extensions.threadarcsjs.timescaling.enabled", false);
+pref("extensions.threadarcsjs.visualisationsize", "12x12,12,12,12,6,-1,1,24");
