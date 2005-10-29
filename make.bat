84974514e6e7b0823c199af5b28478cd8a2ec24f
Alexander C. Hubmann-Haidvogel <ahubmann@gmail.com>
2005-10-29 11:01:09 +0000

    make.bat: - use make.list as list of files to include
    make.list:
    - created list of files to include in package instead
      of defining the files in make.bat

--- make.bat
+++ make.bat
@@ -16,7 +16,7 @@
 @echo Creating XPI file
 @echo ======================================================================
 
-@c:\Programme\7-Zip\7z a -tzip ThreadArcsJS.xpi install.js install.rdf chrome.manifest chrome\threadarcsjs.jar defaults\preferences\ThreadArcsJSDefault.js
+@c:\Programme\7-Zip\7z a -tzip ThreadArcsJS.xpi @make.list
 
 @echo ======================================================================
 @echo Done creating XPI file
