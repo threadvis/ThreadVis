b69321444e59076272c99082cddc5f2577df1cbf
Alexander C. Hubmann-Haidvogel <ahubmann@gmail.com>
2008-12-23 13:26:49 +0000

    ThreadVis is as of now AGPLv3 software. Added copyright & copyleft statements to all source files. Added AGPLv3 text as COPYING file.

--- chrome/content/Container.js
+++ chrome/content/Container.js
@@ -1,12 +1,32 @@
-/** ****************************************************************************
- * Container.js
+/* *****************************************************************************
+ * This file is part of ThreadVis.
+ * http://threadvis.mozdev.org/
  *
- * Copyright (C) 2005-2007 Alexander C. Hubmann
- * Copyright (C) 2007-2008 Alexander C. Hubmann-Haidvogel
+ * ThreadVis started as part of Alexander C. Hubmann-Haidvogel's Master's Thesis
+ * titled "ThreadVis for Thunderbird: A Thread Visualisation Extension for the
+ * Mozilla Thunderbird Email Client" at Graz University of Technology, Austria.
+ * An electronic version of the thesis is available online at
+ * http://www.iicm.tugraz.at/ahubmann.pdf
  *
+ * Copyright (C) 2005, 2006, 2007 Alexander C. Hubmann
+ * Copyright (C) 2007, 2008 Alexander C. Hubmann-Haidvogel
+ *
+ * ThreadVis is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * ThreadVis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with ThreadVis. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Version: $Id$
+ * *****************************************************************************
  * Implements container for messages
- *
- * $Id$
  ******************************************************************************/
 
 
