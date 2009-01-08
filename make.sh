5d32f04c564f99c08fb6e90f3464815a676303cf
Alexander C. Hubmann-Haidvogel <ahubmann@gmail.com>
2009-01-08 15:50:15 +0000

    fix typo

--- make.sh
+++ make.sh
@@ -23,7 +23,7 @@
 # * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
 # * details.
 # *
- #* You should have received a copy of the GNU Affero General Public License
+# * You should have received a copy of the GNU Affero General Public License
 # * along with ThreadVis. If not, see <http://www.gnu.org/licenses/>.
 # *
 # * Version: $Id$
